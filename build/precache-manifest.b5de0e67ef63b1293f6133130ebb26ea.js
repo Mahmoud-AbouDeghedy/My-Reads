2007ac5cf7fb58bf3652441a2111124b716bf8fd
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-03-24 22:25:29 +0200

    buildings

---
+++ build/precache-manifest.b5de0e67ef63b1293f6133130ebb26ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1357f0c0b71079cd6b77",
+    "url": "/static/css/main.4d081a80.chunk.css"
+  },
+  {
+    "revision": "1357f0c0b71079cd6b77",
+    "url": "/static/js/main.1357f0c0.chunk.js"
+  },
+  {
+    "revision": "a9cf79e54d9b50882298",
+    "url": "/static/js/1.a9cf79e5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "0bb0e7882528e88115740397a1845355",
+    "url": "/index.html"
+  }
+];
